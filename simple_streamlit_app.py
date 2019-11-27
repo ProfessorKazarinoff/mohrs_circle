88bf1dd51135360e56566cd163062f3e7cec39f2
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2019-11-26 17:13:33 -0800

    ran black

--- simple_streamlit_app.py
+++ simple_streamlit_app.py
@@ -7,14 +7,14 @@ run the app by installing streamlit with pip and typing
 
 import streamlit as st
 
-st.title('Simple Streamlit App')
+st.title("Simple Streamlit App")
 
-st.text('Type a number in the box below')
+st.text("Type a number in the box below")
 
-n = st.number_input('Number', step=1)
+n = st.number_input("Number", step=1)
 
-st.write(f'{n} + 1 = {n+1}')
+st.write(f"{n} + 1 = {n+1}")
 
-s = st.text_input('Type a name in the box below')
+s = st.text_input("Type a name in the box below")
 
-st.write(f'Hello {s}')
+st.write(f"Hello {s}")
